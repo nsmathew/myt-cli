ed9c801c95b84a89654da9565ba74011ad050853
Nitin Mathew <nitn_mathew2000@hotmail.com>
2020-11-14 14:52:16 +0800

    Added sqlalchemy as install requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     py_modules=['myt'],
     install_requires=[
-        'Click','rich','python-dateutil','mock'
+        'Click','rich','python-dateutil','mock','sqlalchemy'
     ],
     entry_points='''
         [console_scripts]
