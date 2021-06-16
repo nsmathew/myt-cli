da64a2268e5e40df6a0e5dd945ba53ecefe8dd0d
Nitin Mathew <nitn_mathew2000@hotmail.com>
2021-06-16 16:59:30 +0800

    Updated app version to v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='myt-cli',
-    version='0.1.1',
+    version='0.1.2',
     description='myt - My Task Manager',
     py_modules=['myt'],
     install_requires=[
