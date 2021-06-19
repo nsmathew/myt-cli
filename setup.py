0ec4b9af8fa0accc4c0700881b9c61ed55684e25
Nitin Mathew <nitn_mathew2000@hotmail.com>
2021-06-20 00:13:52 +0800

    Updates for the new release - Updated changelog - Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='myt-cli',
-    version='0.1.2',
+    version='0.1.3',
     description='myt - My Task Manager',
     py_modules=['myt'],
     install_requires=[
