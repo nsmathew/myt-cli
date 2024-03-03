5a6442cd495d38f1956205d735a579673274b6bd
Nitin Mathew <penang.notifications@gmail.com>
2024-03-03 18:01:45 +0000

    Added init file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+    myt - my tASK MANAGER
+
+    An application to manage your tasks through the command line using
+    simple options.
+"""
+
+__version__ = "0.1.5"
+__author__ = 'Nitin Mathew'
