43a65839d563fed3707d2b017f1124f4e6fe83e2
Nitin Mathew <penang.notifications@gmail.com>
2024-03-03 14:50:46 +0000

    Update to package versions

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 
 setup(
     name='myt-cli',
-    version='0.1.4',
+    version='0.1.5',
     description='myt - My Task Manager',
     py_modules=['myt'],
     install_requires=[
-        'Click','rich','python-dateutil','mock','sqlalchemy < 1.4',
-        'importlib-metadata ~= 1.0 ; python_version < "3.8"'
+        'Click','rich','python-dateutil','mock','sqlalchemy'
     ],
     entry_points='''
         [console_scripts]
