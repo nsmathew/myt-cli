f0a22ea55e0332d0d2fa8cc44a2ae67130f15d20
Nitin Mathew <penang.notifications@gmail.com>
2024-03-03 17:23:03 +0000

    Version bump, incorrectly mentioned in previous commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='myt-cli',
-    version='0.1.4',
+    version='0.1.5',
     description='myt - My Task Manager',
     author='Nitin Mathew',
     author_email='nitn_mathew2000@hotmail.com',
